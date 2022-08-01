a0fe77003a77df7b9db447a60050e9d6cbc27bd4
wzsamuels <wzsamuels@gmail.com>
2022-07-31 23:40:39 -0400

    Added image to index route.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -21,12 +21,15 @@ export default function IndexRoute() {
     <main className="flex flex-col items-stretch min-h-full h-full">
       <div className="grid grid-cols-1 md:grid-cols-2 pl-4 pr-2">
         <div className='w-full'>
-          <img className="w-full h-auto my-4 pr-2" src="https://twinsilver.mo.cloudinary.net/pizza/pizza2.jpg" alt="Pizza Pic 1"/>
           <img className="w-full my-4 pr-2" src="https://twinsilver.mo.cloudinary.net/pizza/pizza5.jpg?tx=q_auto,f_auto" alt="Pizza Pic 2"/>
+          <img className="w-full h-auto my-4 pr-2" src="https://twinsilver.mo.cloudinary.net/pizza/pizza2.jpg" alt="Pizza Pic 1"/>
         </div>
         <div className='w-full'>
-          <img className="w-full my-4 pl-2" src="https://twinsilver.mo.cloudinary.net/pizza/pizza4.jpg?tx=q_auto,f_auto" alt="Pizza Pic 3"/>
           <img className="w-full my-4 pl-2" src="https://twinsilver.mo.cloudinary.net/pizza/pizza3.jpg?tx=q_auto,f_auto" alt="Pizza Pic 4"/>
+          <img className="w-full my-4 pl-2" src="https://twinsilver.mo.cloudinary.net/pizza/pizza4.jpg?tx=q_auto,f_auto" alt="Pizza Pic 3"/>
+        </div>
+        <div className="w-full col-span-2">
+          <img className="w-full" src="https://twinsilver.mo.cloudinary.net/pizza/pizza6.jpg?tx=q_auto,f_auto" alt="Pizza Pic 4"/>
         </div>
       </div>
 
@@ -35,14 +38,14 @@ export default function IndexRoute() {
         {state !== "success" ?
           <Form method="post" className="my-2" replace>
             <label className="flex my-2 justify-center w-full">
-              <input className="border p-2 mx-2" type="text"/>
+              <input className="border border-gray-500 p-2 mx-2 rounded" type="text"/>
               <button
-                className="border border-gray-500 rounded-2xl px-4 py-2 mx-4 hover:shadow-2xl"
+                className="border border-gray-500 rounded-2xl px-4 py-2 mx-4 bg-red-900 text-white hover:bg-red-700 hover:shadow-2xl "
                 type="submit"
               >
                 {state === 'submitting'
                   ? <span>Subscribing</span>
-                  : <span>Subscribe</span>
+                  : <span className="hover:-translate-y-1">Subscribe</span>
                 }
               </button>
             </label>
